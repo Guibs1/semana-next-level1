f63552e9f8feeba0556261bac2ac7116f58339f2
Guilherme <guilherme@MacBook.local>
2020-06-07 12:24:29 -0300

    adding validations and upload by frontend

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -23,6 +24,24 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 routes.get('/points', pointsContoller.index);
 routes.get('/points/:id', pointsContoller.show);
-routes.post('/points', upload.single('image'), pointsContoller.create);
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    },{
+        abortEarly: false
+    }),
+    pointsContoller.create
+);
 
 export default routes;
